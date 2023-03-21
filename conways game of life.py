d2958a6ada1612ddbc294fe37a804c1748f073e3
IshanEdlabadkar <122237603+IshanEdlabadkar@users.noreply.github.com>
2023-03-21 13:02:27 +0530

    Add files via upload

---
+++ conways game of life.py
@@ -0,0 +1,57 @@
+import copy, random, sys, time
+WIDTH = 79
+HEIGHT = 20
+ALIVE = '0'
+DEAD = ' '
+nextCells = {}
+numNeighbours = 0
+for x in range(WIDTH):
+    for y in range(HEIGHT):
+        if random.randint(0, 1) == 0:
+            nextCells[(x, y )] = ALIVE
+        else:
+            nextCells[(x, y)] = DEAD
+while True:
+    print('\n' * 50)
+    cells = copy.deepcopy(nextCells)
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            print(cells[(x, y)], end='')
+        print()
+    print('Press Ctrl-C to quit')
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            left = (x-1) % WIDTH
+            right = (x+1) % WIDTH
+            above  = (y+1) % HEIGHT
+            below = (y-1) % HEIGHT
+
+            if cells[(left, above)] == ALIVE:
+                numNeighbours += 1
+            if cells[(x, above)] == ALIVE:
+                numNeighbours += 1
+            if cells[(right, above)] == ALIVE:
+                numNeighbours += 1
+            if cells[(left, y)] == ALIVE:
+                numNeighbours += 1
+            if cells[(right, y)] == ALIVE:
+                numNeighbours += 1
+            if cells[(left, below)] == ALIVE:
+                numNeighbours += 1
+            if cells[(x, below)] == ALIVE:
+                numNeighbours += 1
+            if cells[(right, below)] == ALIVE:
+                numNeighbours += 1
+            
+            if cells[(x, y)] == ALIVE and ((numNeighbours==2) or (numNeighbours ==3)):
+                nextCells[x, y] = ALIVE
+            elif cells[(x, y)] == DEAD and numNeighbours == 3:
+                nextCells[x, y] == ALIVE
+            else:
+                nextCells[(x, y)] = DEAD
+    try:
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print("Conway's Game of life")
+        sys.exit()
+    
